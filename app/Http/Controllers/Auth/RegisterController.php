34862742ce88efce0afca955c8d32ac496a53be9
Kaishiyoku <dmp@nfze.de>
2018-06-03 15:06:33 +0200

    added user creation Artisan command

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -59,9 +59,14 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Models\User
      */
     protected function create(array $data)
+    {
+        return self::createUser($data);
+    }
+
+    public static function createUser(array $data)
     {
         return User::create([
             'name' => $data['name'],
